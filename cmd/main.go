37c3e3c2ea80c4dc471840668babedd9f3369ce1
taiti09 <taitiabe1997@gmail.com>
2024-08-03 12:08:06 +0900

    linterの指摘修正

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,10 @@ package main
 import "gofr.dev/pkg/gofr"
 
 func main() {
-	// initialise gofr object
+	// initialize gofr object
 	app := gofr.New()
-
 	// register route greet
 	app.GET("/greet", func(ctx *gofr.Context) (interface{}, error) {
-
 		return "Hello World!", nil
 	})
 
