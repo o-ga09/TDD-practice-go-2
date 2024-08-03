3c69249687c248ab06188819cec5e65e0405fa83
taiti09 <taitiabe1997@gmail.com>
2024-08-03 21:26:56 +0900

    serviceを実装

--- services/note/note.go
+++ services/note/note.go
@@ -1,1 +1,77 @@
 package note
+
+import (
+	"context"
+
+	"github.com/o-ga09/note-app-backendapi/domain"
+)
+
+type NoteService struct {
+	noteRepo domain.INoteRepository
+}
+
+func NewNoteService(noteRepo domain.INoteRepository) *NoteService {
+	return &NoteService{
+		noteRepo: noteRepo,
+	}
+}
+
+func (s *NoteService) FetchNoteById(ctx context.Context, id string) (*domain.Note, error) {
+	res, err := s.noteRepo.GetNoteById(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+func (s *NoteService) FetchNotes(ctx context.Context) ([]*domain.Note, error) {
+	res, err := s.noteRepo.GetNotes(ctx)
+	if err != nil {
+		return nil, err
+	}
+	notes := make([]*domain.Note, 0)
+	for _, note := range res {
+		note := domain.Note{
+			NoteID:    note.NoteID,
+			Title:     note.Title,
+			Content:   note.Content,
+			CreatedAt: note.CreatedAt,
+			UpdatedAt: note.UpdatedAt,
+		}
+		notes = append(notes, &note)
+	}
+	return notes, nil
+}
+
+func (s *NoteService) CreateNote(ctx context.Context, title, content string) error {
+	note := domain.Note{
+		Title:   title,
+		Content: content,
+	}
+	err := s.noteRepo.CreateNote(ctx, note)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *NoteService) UpdateNote(ctx context.Context, id, title, content string) error {
+	note := domain.Note{
+		NoteID:  id,
+		Title:   title,
+		Content: content,
+	}
+	err := s.noteRepo.UpdateNote(ctx, note)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *NoteService) DeleteNoteById(ctx context.Context, id string) error {
+	err := s.noteRepo.DeleteNoteById(ctx, id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ services/user/user.go
@@ -0,0 +1,77 @@
+package user
+
+import (
+	"context"
+
+	"github.com/o-ga09/note-app-backendapi/domain"
+)
+
+type UserService struct {
+	userRepo domain.IUserRepository
+}
+
+func NewUserService(userRepo domain.IUserRepository) *UserService {
+	return &UserService{
+		userRepo: userRepo,
+	}
+}
+
+func (s *UserService) FetchUserById(ctx context.Context, id string) (*domain.User, error) {
+	res, err := s.userRepo.GetUserById(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+func (s *UserService) FetchUsers(ctx context.Context) ([]*domain.User, error) {
+	res, err := s.userRepo.GetUsers(ctx)
+	if err != nil {
+		return nil, err
+	}
+	users := make([]*domain.User, 0)
+	for _, user := range res {
+		user := domain.User{
+			UserID:    user.UserID,
+			Username:  user.Username,
+			UserEmail: user.UserEmail,
+			CreatedAt: user.CreatedAt,
+			UpdatedAt: user.UpdatedAt,
+		}
+		users = append(users, &user)
+	}
+	return users, nil
+}
+
+func (s *UserService) CreateUser(ctx context.Context, username, userEmail string) error {
+	user := domain.User{
+		Username:  username,
+		UserEmail: userEmail,
+	}
+	err := s.userRepo.CreateUser(ctx, user)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *UserService) UpdateUser(ctx context.Context, id, username, userEmail string) error {
+	user := domain.User{
+		UserID:    id,
+		Username:  username,
+		UserEmail: userEmail,
+	}
+	err := s.userRepo.UpdateUser(ctx, user)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *UserService) DeleteUserById(ctx context.Context, id string) error {
+	err := s.userRepo.DeleteUserById(ctx, id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
